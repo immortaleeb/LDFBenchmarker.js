955f154019cf5291725f28448eaa1c635ab55622
Miel Vander Sande <miel.vandersande@ugent.be>
2015-04-17 17:36:35 +0200

    Prepared test

--- evaluation.sh
+++ evaluation.sh
@@ -1,29 +1,60 @@
 
 npm install
+npm install forever -g
+
 mkdir output
 
-#while :; do top -p $(pgrep -d',' http) -n 1 >> log.txt; date >> log.txt; sleep 1; done
+# Prepare client
+git submodule init
+git submodule update
+#npm link clients/ldf-client
+ln -s ../clients/ldf-client/ ./node_modules/ldf-client
 
-timeout=60000
+# Set configuration
+timeout=90000
+dataset=watdiv100M
 
-#node slow-proxy.js 3000 3001 100
+port_original=3000
+pid_original=2096
+
+port_original_delayed=3001
+pid_original_delayed=3025
+
+port_amq=3002
+pid_amq=2117
+
+port_amq_delayed=3003
+pid_amq_delayed=3050
 
 #Start monitoring servers
-./monitor 
+forever ./monitor $pid_original > ./output/server_$port_original.csv
+forever ./monitor $pid_amq > ./output/server_$port_amq.csv
 
 function run {
   echo "--- Run $1 ---"
-  ./run-tests.js ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:3000/watdiv100M $timeout ldf-client > ./output/$1.csv
-  ./run-tests.js ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:3001/watdiv100M $timeout ldf-client > ./output/$1-delayed.csv
-  ./run-tests.js ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:3002/watdiv100M $timeout ldf-client-amq > ./output/$1-amq.csv
-  ./run-tests.js ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:3003/watdiv100M $timeout ldf-client-amq > ./output/$1-amq-delayed.csv
-  ./run-tests.js ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:3002/watdiv100M $timeout ldf-client-joachim > ./output/$1-amq.csv
-  ./run-tests.js ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:3003/watdiv100M $timeout ldf-client-joachim > ./output/$1-amq-delayed.csv
+  # Original setup
+  cd clients/ldf-client; git checkout master; npm install --production; cd ../..
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_original/$dataset $timeout > ./output/$1.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_original_delayed/$dataset $timeout ldf-client > ./output/$1-delayed.csv
+  # AMQ setup
+  cd clients/ldf-client; git checkout amq; npm install --production; cd ../..
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq/$dataset $timeout > ./output/$1-amq.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq_delayed/$dataset $timeout > ./output/$1-amq-delayed.csv
+  # optimized setup
+  cd clients/ldf-client; git checkout query-optimization; npm install --production; cd ../..
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_original/$dataset $timeout optimized > ./output/$1-optimized.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_original_delayed/$dataset $timeout optimized > ./output/$1-optimized-delayed.csv
+  # combined setup
+  cd clients/ldf-client; git checkout query-optimization-amq; npm install --production; cd ../..
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq/$dataset $timeout > ./output/$1-optimized-amq.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq_delayed/$dataset $timeout > ./output/$1-optimized-amq-delayed.csv
 }
 
-run "warmup"
-#run "test.1"
+#run "warmup"
+run "test.1"
 #run "test.2"
 #run "test.3"
 #run "test.4"
 #run "test.5"
+
+forever stopall
