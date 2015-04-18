b11c3d98fadc71f5be22dd5a0842aa7f2f72ed3c
Miel Vander Sande <miel.vandersande@ugent.be>
2015-04-18 11:45:50 +0200

    Completed setup

--- evaluation.sh
+++ evaluation.sh
@@ -26,9 +26,16 @@ pid_amq=2117
 port_amq_delayed=3003
 pid_amq_delayed=3050
 
+port_gcs=3004
+pid_gcs=2117
+
+port_gcs_delayed=3005
+pid_gcs_delayed=3050
+
 #Start monitoring servers
-forever ./monitor $pid_original > ./output/server_$port_original.csv
-forever ./monitor $pid_amq > ./output/server_$port_amq.csv
+./monitor $pid_original > ./output/server_$port_original.csv &
+./monitor $pid_amq > ./output/server_$port_amq.csv &
+./monitor $pid_gcs > ./output/server_$port_amq.csv &
 
 function run {
   echo "--- Run $1 ---"
@@ -40,6 +47,8 @@ function run {
   cd clients/ldf-client; git checkout amq; npm install --production; cd ../..
   ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq/$dataset $timeout > ./output/$1-amq.csv
   ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq_delayed/$dataset $timeout > ./output/$1-amq-delayed.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_gcs/$dataset $timeout > ./output/$1-gcs.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_gcs_delayed/$dataset $timeout > ./output/$1-gcs-delayed.csv
   # optimized setup
   cd clients/ldf-client; git checkout query-optimization; npm install --production; cd ../..
   ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_original/$dataset $timeout optimized > ./output/$1-optimized.csv
@@ -48,6 +57,8 @@ function run {
   cd clients/ldf-client; git checkout query-optimization-amq; npm install --production; cd ../..
   ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq/$dataset $timeout > ./output/$1-optimized-amq.csv
   ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_amq_delayed/$dataset $timeout > ./output/$1-optimized-amq-delayed.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_gcs/$dataset $timeout > ./output/$1-optimized-gcs.csv
+  ./run-tests ./stress-workloads/watdiv-stress-100/$1.sparql http://localhost:$port_gcs_delayed/$dataset $timeout > ./output/$1-optimized-gcs-delayed.csv
 }
 
 #run "warmup"
